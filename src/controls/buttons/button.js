9fddab1aaa39b8baa1e51f80ed0906c6972ee529
Alex Iskander <alex@tpsitulsa.com>
2010-03-02 09:52:00 -0600

    New and improved button renderer :)

--- src/controls/buttons/button.js
+++ src/controls/buttons/button.js
@@ -1,39 +1,15 @@
 require("src/theme");
 
 SC.AceTheme.renderers.Button = SC.EmptyTheme.renderers.Button.extend({
-  render: function(context, firstTime) {
-    // add href attr if tagName is anchor...
-    var href, toolTip, classes;
-    if (this.tagName === 'a') {
-      href = this.href;
-      if (!href || (href.length === 0)) href = "javascript"+":;";
-      context.attr('href', href);
-    }
-
-    // If there is a toolTip set, grab it and localize if necessary.
-    toolTip = this.toolTip;
-    if (SC.typeOf(toolTip) === SC.T_STRING) {
-      if (this.localize) toolTip = toolTip.loc() ;
-      context.attr('title', toolTip) ;
-      context.attr('alt', toolTip) ;
-    }
-  
-    // add some standard attributes & classes.
-    classes = this._TEMPORARY_CLASS_HASH || {};
-    classes.def = this.isDefault;
-    classes.cancel = this.isCancel;
-    classes.icon = this.isIcon;
-    context.attr('role', 'button')
-      .setClass(classes).addClass(this.theme);
-  
+  renderContents: function(context) {
     // render background slices
-    if (!context.hasElement()) {
-      context.push("<span class='button-left'></span>");
-      context.push("<span class='button-right'></span>");
-      context.push("<span class='button-middle'></span>");
-    }
+    context.push("<span class='button-left'></span>");
+    context.push("<span class='button-right'></span>");
+    context.push("<span class='button-middle'></span>");
   
     // render inner html
-    this.renderTitle(context, firstTime);
+    this._titleRenderer.render(context);
   }
 });
+
+SC.AceTheme.renderers.button = SC.AceTheme.renderers.Button.create();
